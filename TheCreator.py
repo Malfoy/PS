f847fa23478b4f1e48fa989df2ce3792804f016d
Malfoy <antoine.limasse@gmail.com>
2016-06-09 11:42:38 +0200

    thecreator was not pyhton

--- TheCreator.py
+++ TheCreator.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
-import subprocess,sys,struct,os,time,shutil,random
-
+import subprocess,sys,struct,os,time,shutil
+from random import *
 if len(sys.argv) < 6:
 	sys.exit( 'Usage : [Ref file] [Size read] [Error rate] [output file] [Number of read]' )
 
 debut=time.time()
 
-refFile=sys.argv[1]
-sizeRead=sys.argv[2]
-errorRate=sys.argv[3]
+#~ refFile=sys.argv[1]
+sizeRead=int(sys.argv[2])
+errorRate=int(sys.argv[3])
 outputFile=sys.argv[4]
-nRead=sys.argv[4]
+nRead=int(sys.argv[5])
 
 out = open(outputFile, 'w')
 out.truncate()
@@ -18,33 +18,35 @@ out.truncate()
 
 def randNuc(nuc):
 	rand=randint(0,4)
-	if(rand==0 and nuc!='A')
+	if(rand==0 and nuc!='A'):
 		return 'A'
-	if(rand==0 and nuc!='C')
+	if(rand==1 and nuc!='C'):
 		return 'C'
-	if(rand==0 and nuc!='G')
+	if(rand==2 and nuc!='G'):
 		return 'G'
-	if(rand==0 and nuc!='T')
+	if(rand==3 and nuc!='T'):
 		return 'T'
+	return randNuc(nuc)
 
 
 with open(sys.argv[1]) as refFile:
-    useless = refFile.readline()
-    ref = refFile.readline()
-
-
-for num in range(0,nRead):
-	position=(randint(0,ref.size()-sizeRead))
-	readRef=ref[position:sizeRead]
-	readRef.upper()
-	readErr=readRef
-	readRef=">"+readRef
-	i=0
-	for c in readErr:
-		i+=1
-		if(randint(0,errorRate)==0)
-			readErr=readErr[:i]+randNuc(c)+readErr[i+1:]
-	out.write(readRef)
-	out.write("\n")
-	out.write(readErr)
-	out.write("\n")
+	useless = refFile.readline()
+	ref = refFile.readline()
+	for num in range(0,nRead):
+		position=(randint(0,len(ref)-sizeRead))
+		#~ print (position)
+		readRef=ref[position:position+sizeRead]
+		#~ print(readRef)
+		#~ wait = input("PRESS ENTER TO CONTINUE.")
+		#~ readRef.upper()
+		#~ readErr=readRef
+		#~ readRef=">"+readRef
+		i=0
+		#~ for c in readErr:
+			#~ i+=1
+			#~ if(randint(0,errorRate)==0):
+				#~ readErr=readErr[:i]+randNuc(c)+readErr[i+1:]
+		#~ out.write(readRef)
+		out.write(">lol\n")
+		out.write(readRef)
+		out.write("\n")
